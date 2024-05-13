e4a966bddbfa83e3af1572d0faf7cee2e81932d7
AT <68025214+Fatmira@users.noreply.github.com>
2024-05-13 20:05:32 +0200

    Create design.bat

---
+++ design.bat
@@ -0,0 +1,56 @@
+title dr
+@echo off
+color a
+cls
+
+echo   .:'                                  `:.                                    
+echo  ::'                                     `::                                   
+echo :: :.                                  .: ::                                  
+echo :
+echo  `:. `:.             .             .:'  .:'                                   
+echo   `::. `::           !           ::' .::'           
+echo       `::.`::.    .' ! `.    .::'.::'        
+echo         `:.  `::::'':!:``::::'   ::'                                            
+echo         :' :::.  .:' ! `:.  .::: `:  
+echo        :: HHH::.   ` ! '   .::HHH ::                **************                          
+echo       ::: `H TH::.  `!'  .::HT H' :::               *            *                      
+echo       ::..  `THHH:`:   :':HHHT'  ..::               *  Fatmira	 *            
+echo       `::      `T: `. .' :T'      ::'               *            *             
+echo         `:. .   :         :   . .:'                 **************                    
+echo           `::'               `::'                                             
+echo             :'  .`.  .  .'.  `:                                               
+echo             :' ::.       .:: `:                                              
+echo             :' `:::     :::' `:                                           
+echo              `.  ``     ''  .'                                           
+echo               :`...........':                                                 
+echo               ` :`.     .': '           
+echo                `:  `"""'  :' 
+echo:                               
+:loop
+color 1
+ping localhost -n 1 >nul
+timeout /t 1 /nobreak >nul
+color 2
+timeout /t 1 /nobreak >nul
+color 3
+timeout /t 1 /nobreak >nul
+color 4
+timeout /t 1 /nobreak >nul
+color 5
+timeout /t 1 /nobreak >nul
+color 6
+timeout /t 1 /nobreak >nul
+color b
+timeout /t 1 /nobreak >nul
+color c
+timeout /t 1 /nobreak >nul
+color e
+timeout /t 1 /nobreak >nul
+color 9
+timeout /t 1 /nobreak >nul
+color d
+timeout /t 1 /nobreak >nul
+color a
+timeout /t 1 /nobreak >nul
+goto loop
+pause >nul
